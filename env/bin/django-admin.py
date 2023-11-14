ffcd8127f57fb2b577d6998473de70c4e4a0bd38
Mishank Kumar <mishank2002@gmail.com>
2023-11-15 01:05:43 +0530

    Files Added
    Files Added

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mishankkumar/Desktop/Machine-Learning-Disease-Prediction-And-Drug-Recommendation-main/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
